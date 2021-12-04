de86a98ad530e7841271a6bf1532bb4a6feff87a
luciano ereu <71347892+lucianoereu@users.noreply.github.com>
2021-12-03 21:14:12 -0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,16 +1,26 @@
 /* constante */
-const recetaPan =[{ id: 1, ingrediente: "Harina", cantidad: "5", unidad: "gr" },
-                  { id: 2, ingrediente: "agua", cantidad: "3", unidad: "gr" },
-                  { id: 3, ingrediente: "masa Madre", cantidad: "2", unidad: "gr" },
-                  { id: 4, ingrediente: "sal", cantidad: "1", unidad: "gr" },]
+const recetaPan = [{ id: 1, ingrediente: "Harina", cantidad: 5, unidad: "gr" },
+    { id: 2, ingrediente: "agua", cantidad: 3, unidad: "gr" },
+    { id: 3, ingrediente: "masa Madre", cantidad: 2, unidad: "gr" },
+    { id: 4, ingrediente: "sal", cantidad: 1, unidad: "gr" },
+]
+
+
 
 /* ingreso de cantidad */
 let cantidadComensal = parseInt(prompt("ingre la cantidad de comensales"));
-// funcion
-for(let recetaCantidad of recetaPan){
-    alert(`${recetaCantidad.ingrediente} ${recetaCantidad.cantidad} ${recetaCantidad.unidad}`);
+
+let total = recetaPan.map(ingrediente => ingrediente.cantidad * cantidadComensal);
+
+function mostrarIngredientes(receta) {
+    receta.forEach(ingrediente => {
+        alert(`${ingrediente.ingrediente} ${total.cantidad} ${ingrediente.unidad}`);
+    });
 }
+mostrarIngredientes(recetaPan);
 
 /* buscar el elemento "Masa madre" */
-const resultado = recetaPan.find( ingredientes => ingredientes.ingrediente === "masa Madre" );
+const resultado = recetaPan.find(ingredientes => ingredientes.ingrediente === "masa Madre");
 console.log(resultado);
+
+
