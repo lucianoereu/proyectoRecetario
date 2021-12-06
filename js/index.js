63d01fd4aafad4982c41bab3145bf4c3b4c48934
luciano ereu <71347892+lucianoereu@users.noreply.github.com>
2021-12-06 20:19:01 -0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -5,13 +5,13 @@ const recetaPan = [{ id: 1, ingrediente: "Harina", cantidad: 5, unidad: "gr" },
     { id: 4, ingrediente: "sal", cantidad: 1, unidad: "gr" },
 ]
 
-
-
 /* ingreso de cantidad */
 let cantidadComensal = parseInt(prompt("ingre la cantidad de comensales"));
 
+/* creo array para la cantidad de cada ingrediente segun la cantidad de comensales ingresada */
 let total = recetaPan.map(ingrediente => ingrediente.cantidad * cantidadComensal);
 
+/* realizzo un ciclo for donde imprimo la cantidad de TOTAL  */
 for (let i = 0; i < total.length; i++) {
     alert(recetaPan[i].ingrediente + " " + total[i] + " " + recetaPan[i].unidad);
 }
