08eec528d5618c13a17cfdc49625a5a79bc45b2b
luciano ereu <71347892+lucianoereu@users.noreply.github.com>
2021-12-27 17:33:44 -0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,23 +1,28 @@
 /* constante */
-const recetaPan = [{ id: 1, ingrediente: "Harina", cantidad: 5, unidad: "gr" },
-    { id: 2, ingrediente: "agua", cantidad: 3, unidad: "gr" },
-    { id: 3, ingrediente: "masa Madre", cantidad: 2, unidad: "gr" },
-    { id: 4, ingrediente: "sal", cantidad: 1, unidad: "gr" },
+const recetaPan = [{ id: 1, porcentaje: "90%", ingrediente: "Harina", cantidad: 47, unidad: "gr" },
+    { id: 2, porcentaje: "70%", ingrediente: "agua", cantidad: 36, unidad: "gr" },
+    { id: 3, porcentaje: "20%", ingrediente: "masa Madre", cantidad: 10, unidad: "gr" },
+    { id: 4, porcentaje: "2%", ingrediente: "sal", cantidad: 1, unidad: "gr" },
 ]
+$(document).ready(function() {
+    /* ingreso de cantidad */
+    let cantidadComensal = document.getElementById("cantidad");
 
-/* ingreso de cantidad */
-let cantidadComensal = document.getElementById("cantidad");
+
+    /* imprimir en documento por getElementById */
+    let cambio = document.getElementById("panMM");
+    /* usando jquery */
+    $("#cantidad").on("keyup", function() {
+        /* comento el add para imprimir en el dom */
+        /* cantidadComensal.addEventListener("keyup", () => { */
+        /* creo array para la cantidad de cada ingrediente segun la cantidad de comensales ingresada */
+        let total = recetaPan.map(ingrediente => ingrediente.cantidad * cantidadComensal.value);
+        cambio.innerHTML = recetaPan[0].ingrediente + " " + total[0] + " " + recetaPan[0].unidad + "<br>" + recetaPan[1].ingrediente + " " + total[1] + " " + recetaPan[1].unidad + "<br>" + recetaPan[2].ingrediente + " " + total[2] + " " + recetaPan[2].unidad + "<br>" + recetaPan[3].ingrediente + " " + total[3] + " " + recetaPan[3].unidad
+    })
 
 
-/* imprimir en documento por getElementById */
-let cambio = document.getElementById("panMM");
+    /*         /* ordenar por orden alfabetico de ingredientes */
+    let ordenar = recetaPan.sort((a, b) => a.ingrediente.localeCompare(b.ingrediente));
+    console.log(ordenar);
 
-cantidadComensal.addEventListener("keyup", () => {
-    /* creo array para la cantidad de cada ingrediente segun la cantidad de comensales ingresada */
-    let total = recetaPan.map(ingrediente => ingrediente.cantidad * cantidadComensal.value);
-    cambio.innerHTML = recetaPan[0].ingrediente + " " + total[0] + " " + recetaPan[0].unidad + "<br>" + recetaPan[1].ingrediente + " " + total[1] + " " + recetaPan[1].unidad + "<br>" + recetaPan[2].ingrediente + " " + total[2] + " " + recetaPan[2].unidad + "<br>" + recetaPan[3].ingrediente + " " + total[3] + " " + recetaPan[3].unidad
-})
-
-/*         /* ordenar por orden alfabetico de ingredientes */
-let ordenar = recetaPan.sort((a, b) => a.ingrediente.localeCompare(b.ingrediente));
-console.log(ordenar);
+});
