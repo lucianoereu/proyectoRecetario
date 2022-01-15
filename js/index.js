672da8813a9050f4c15f50ae57b8e5160949ce58
luciano ereu <71347892+lucianoereu@users.noreply.github.com>
2022-01-15 19:36:10 -0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 /* animaciones */
 $("#panMM, #pizza, #focaccia, #arepas").hide("fast");
+
 $("#btnPanMM").click(() => {
     $("#panMM").slideToggle();
 });
@@ -12,11 +13,11 @@ $("#btnFocaccia").click(() => {
 $("#btnArepa").click(() => {
     $("#arepas").slideToggle();
 });
-/* llamada dinamica */
-let ingredientesJson = "";
+
 
 /* llamada dianmica del json */
 
+let ingredientesJson = "";
 $.ajax({
     url: "js/ingredientes.json",
     dataType: "json",
@@ -67,16 +68,26 @@ $("#cantidadFocaccia").on("keyup", function() {
 /* calcular y mostrar arepas */
 $("#cantidadArepa").on("keyup", function() {
     let arepas = ingredientesJson.ingredientesArepas;
+    console.log(arepas);
     let total = arepas.map(ingrediente => ingrediente.cantidad * cantidadArepas.value);
     cambio4.innerHTML = arepas[0].ingrediente + " " + total[0] + " " + arepas[0].unidad + "<br>" + arepas[1].ingrediente + " " + total[1] + " " + arepas[1].unidad + "<br>" + arepas[2].ingrediente + " " + total[2] + " " + arepas[2].unidad + "<br>" + arepas[3].ingrediente + " " + total[3] + " " + arepas[3].unidad
 });
 
 
-
+/* let receta = "";
+$.ajax({
+    url: "recetas/recetas.json",
+    dataType: "text",
+    success: (respuesta) => {
+        receta = respuesta;
+    },
+}); */
 
 /* ajax */
+/* traemos receta pan masa madre */
+document.querySelector("#mostrarRecetaMM").addEventListener("click", mostrarReceta);
 
-document.querySelector("#mostrarReceta").addEventListener("click", function mostrarReceta() {
+function mostrarReceta() {
     /* console.log("mostrar recetas"); */
     const xhttp = new XMLHttpRequest();
     xhttp.open("GET", "recetas/recetaMM.txt", true);
@@ -84,7 +95,43 @@ document.querySelector("#mostrarReceta").addEventListener("click", function most
 
     xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status === 200) {
-            document.getElementById("receta").innerHTML = this.responseText;
+            document.getElementById("recetaMM").innerHTML = this.responseText;
         }
     }
-})
+    $("#recetaMM").slideToggle();
+}
+
+/* traemos receta pizza */
+
+document.querySelector("#mostrarRecetaPizza").addEventListener("click", mostrarRecetaPizza);
+
+function mostrarRecetaPizza() {
+    /* console.log("mostrar recetas"); */
+    const xhttp = new XMLHttpRequest();
+    xhttp.open("GET", "recetas/recetaPizza.txt", true);
+    xhttp.send();
+
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status === 200) {
+            document.getElementById("recetaPizza").innerHTML = this.responseText;
+        }
+    }
+    $("#recetaPizza").slideToggle();
+}
+
+/* traemos la recceta de focaccia */
+document.querySelector("#mostrarRecetaFocaccia").addEventListener("click", mostrarRecetaFocaccia);
+
+function mostrarRecetaFocaccia() {
+    /* console.log("mostrar recetas"); */
+    const xhttp = new XMLHttpRequest();
+    xhttp.open("GET", "recetas/recetaFocaccia.txt", true);
+    xhttp.send();
+
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status === 200) {
+            document.getElementById("recetaFocaccia").innerHTML = this.responseText;
+        }
+    }
+    $("#recetaFocaccia").slideToggle();
+}
